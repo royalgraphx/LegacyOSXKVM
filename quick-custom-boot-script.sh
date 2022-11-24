a10a59fde9e926b6846790e792d3d351a72fbe9e
RoyalGraphX <rgx@rgx>
2022-11-24 02:29:01 -0600

    Include "Quick" Scripts

---
+++ quick-custom-boot-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo -e 'qemu-system-x86_64 -machine pc-q35-6.1,vmport=off,accel=kvm -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
+-name Custom Mac OS X Enviroment \
+-uuid 22467C52-A237-4521-6676-FFB147945215C \
+-vga none \
+-device vmware-svga \
+-device virtio-rng-pci \
+-device e1000,mac=8A:A8:6A:F8:00:51,netdev=net0 \
+-netdev user,id=net0 \
+-cpu Penryn,vendor=GenuineIntel,+sse4.1,+sse4.2,+ssse3 \
+-smp 4,cores=2 \
+-device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
+-m 4G \
+-device ich9-intel-hda \
+-device hda-duplex \
+-usb \
+-device usb-tablet,bus=usb-bus.0 \
+-device usb-mouse,bus=usb-bus.0 \
+-device usb-kbd,bus=usb-bus.0 \
+-device ide-hd,bus=ide.0,drive=OpenCore,bootindex=0 \
+-drive "if=none,format=raw,media=disk,id=OpenCore,file=opencore/opencore.qcow2,discard=unmap,detect-zeroes=unmap" \
+-device ide-hd,bus=ide.2,drive=HardDrives,bootindex=2 \
+-drive "if=none,format=raw,media=disk,id=HardDrives,file=harddrives/.img,discard=unmap,detect-zeroes=unmap" \
+'  >> Boot.sh
+
+chmod +x Boot.sh
