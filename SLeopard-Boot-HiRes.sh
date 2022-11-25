53a6ecedabb79662ecc5b8f26fe20eeee69f499f
RoyalGraphX <rgx@rgx>
2022-11-25 00:22:52 -0600

    Update MLion OpenCore!

--- SLeopard-Boot-HiRes.sh
+++ SLeopard-Boot-HiRes.sh
@@ -2,11 +2,11 @@
 
 qemu-system-x86_64 -machine pc-q35-6.1,vmport=off,accel=kvm -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
 -name "Snow Leopard" \
--uuid 3159DCE2-A537-47C1-B2C6-FFB44069788C \
+-uuid 3259DCE2-A537-47C1-B2C6-FFB44069788C \
 -vga none \
 -device vmware-svga \
 -device virtio-rng-pci \
--device e1000,mac=8A:A8:6A:F8:00:51,netdev=net0 \
+-device e1000,mac=EE:E9:D3:14:05:CF,netdev=net0 \
 -netdev user,id=net0 \
 -cpu Penryn,vendor=GenuineIntel,+sse4.1,+sse4.2,+ssse3 \
 -smp 8,cores=4 \
@@ -22,3 +22,9 @@ qemu-system-x86_64 -machine pc-q35-6.1,vmport=off,accel=kvm -bios /usr/share/edk
 -drive "if=none,format=raw,media=disk,id=OpenCore,file=cdrom/1920x1080-OpenCore.img,discard=unmap,detect-zeroes=unmap" \
 -device ide-hd,bus=ide.2,drive=HardDrives,bootindex=2 \
 -drive "if=none,format=raw,media=disk,id=HardDrives,file=harddrives/macintosh.img,discard=unmap,detect-zeroes=unmap" \
+-device ide-hd,bus=ide.3,drive=OSXInstaller,bootindex=3 \
+-drive "if=none,format=raw,media=disk,id=OSXInstaller,file=osxinstaller/MLion.img,discard=unmap,detect-zeroes=unmap" \
+-device ide-hd,bus=ide.4,drive=OpenCoreML,bootindex=4 \
+-drive "if=none,media=disk,id=OpenCoreML,file=opencore/opencore-MLion.qcow2,discard=unmap,detect-zeroes=unmap" \
+-device ide-hd,bus=ide.5,drive=CDROM,bootindex=5 \
+-drive "if=none,format=raw,media=disk,id=CDROM,file=../Muklti.iso,discard=unmap,detect-zeroes=unmap" \
